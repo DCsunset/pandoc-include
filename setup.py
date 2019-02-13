d3928527e266b8144e668acf600a2f53be4466a5
DCsunset <DCsunset@users.noreply.github.com>
2019-02-13 20:12:39 +0800

    Add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
+from os import path
 
-VERSION = '0.1.0'
+version = '0.1.0'
+
+# Long description
+with open(path.join(path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='pandoc-include',
-    version=VERSION,
+    version=version,
     description='Panflute filter to allow file includes',
+    long_description=long_description
     author='DCsunset',
     author_email='DCsunset@protonmail.com',
     license='MIT',
