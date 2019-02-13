a1c8929bd92f454aed7195459337a8dc3f41d460
DCsunset <DCsunset@users.noreply.github.com>
2019-02-13 18:09:58 +0800

    Fix the filename bug in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
 
+VERSION = '0.1.0'
+
 setup(
     name='pandoc-include',
-    version='0.1.0',
+    version=VERSION,
     description='Panflute filter to allow file includes',
     author='DCsunset',
+    author_email='DCsunset@protonmail.com',
     license='MIT',
     url='https://github.com/DCsunset/pandoc-include',
     
     install_requires=['panflute>=1'],
     entry_points={
         'console_scripts': [
-            'pandoc-include = pandoc-include:main'
+            'pandoc-include = pandoc_include:main'
         ]
     },
 )
